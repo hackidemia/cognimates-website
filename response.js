a8009dbe1144cf8b3aeb8f07d8b82fa73b56696b
Tammy Qiu <tammy.qiu@yahoo.com>
2018-04-20 19:40:21 -0400

    nav edit

--- response.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("siteNav");
-    if (x.className === "site-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "site-nav";
-    }
-}
